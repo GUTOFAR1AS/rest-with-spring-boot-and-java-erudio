e87443c0be4ec6ee703c8a32cb6d1d743590dbf7
Gustavo <tekkli4play@outlook.com>
2024-09-30 08:19:55 -0300

    Implementando Content Negotiation via QUERY Parameter - AJUSTADO

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,38 +1,24 @@
 package br.com.erudio.config;
 
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public MappingJackson2HttpMessageConverter jsonMessageConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT); // Formato legível para JSON
-        return converter;
-    }
-
-    @Bean
-    public MappingJackson2XmlHttpMessageConverter xmlMessageConverter() {
-        MappingJackson2XmlHttpMessageConverter converter = new MappingJackson2XmlHttpMessageConverter();
-        converter.setObjectMapper(new XmlMapper()); // Usar XmlMapper para conversão XML
-        return converter;
-    }
-
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(jsonMessageConverter());
-        converters.add(xmlMessageConverter());
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // Ativando Content Negotiation via Query Parameter
+        configurer
+            .favorParameter(true) // Ativa o uso de query parameter
+            .parameterName("mediaType") // Define o nome do parâmetro (pode ser 'format' ou 'mediaType')
+            .ignoreAcceptHeader(true) // Ignora o cabeçalho 'Accept' e prioriza o query parameter
+            .useRegisteredExtensionsOnly(false)
+            .defaultContentType(org.springframework.http.MediaType.APPLICATION_JSON) // Tipo de conteúdo padrão
+            .mediaType("json", org.springframework.http.MediaType.APPLICATION_JSON) // Mapeia 'json' para JSON
+            .mediaType("xml", org.springframework.http.MediaType.APPLICATION_XML); // Mapeia 'xml' para XML
     }
 }
